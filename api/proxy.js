f1a3c7c2e9bb5c66ad18f5d490487e98c1b06d61
kkkkyrie <2323740041@qq.com>
2022-11-07 21:07:15 +0800

    api

--- api/proxy.js
+++ api/proxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target
+  console.log(req.url)
   if (req.url.startsWith('/base')) {
     target = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
   }
